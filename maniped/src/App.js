7f8b5dc332142b6cdc2cdfddadaf7082b787f1ea
Ali Rajaii <rajaii_a@yahoo.com>
2020-07-08 17:12:46 -0400

    added Nav and 2 main colors picked for nav and background

--- maniped/src/App.js
+++ maniped/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
+import Nav from './components/Nav.js';
+import HomePage from './components/HomePage.js';
 import './App.css';
 
 class App extends React.Component {
  render() {
    return (
-   <div>
-     hi
-   </div>
+     <div>
+        <Nav/>
+        <Route path='/' exact render={(props) => <HomePage {...props} />} ></Route>
+        
+
+       
+    </div>
    )
  }
 
+        // about, services, login, signup,
  
 }
 
--- maniped/src/index.js
+++ maniped/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
