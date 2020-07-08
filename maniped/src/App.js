db88693ad115d278f707b7fd9ff93063ff7e79ad
Ali Rajaii <rajaii_a@yahoo.com>
2020-07-08 16:08:10 -0400

    index.js configured to redux and router, actions and reducers dirs added

--- maniped/src/App.js
+++ maniped/src/App.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import axios from 'axios';
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
+ render() {
+   return (
+   <div>
+     hi
+   </div>
+   )
+ }
 
-  const handleSubmit = (pic) => {
-    axios.post('http://localhost:3000/api/avatars', pic)
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }
-  return (
-    <div>
-      <form>
-        <label for="avatar">Choose a profile picture:</label>
-
-        <input type="file"
-        id="avatar" name="avatar"></input>
-        <button onClick={() => handleSubmit()}></button>
-      </form>
-    </div>
-
-  );
+ 
 }
 
 export default App;
