03d45dff064a5e999bed78310aec55e89e5e749d
Ali Rajaii <rajaii_a@yahoo.com>
2020-08-10 18:44:06 -0400

    working, now just get button to render on change for privacy or sms

--- maniped/src/components/Dashboard/FeedSub.js
+++ maniped/src/components/Dashboard/FeedSub.js
@@ -17,9 +17,23 @@ class FeedSub extends React.Component {
             editingProfile: false,
             sms: null,
             privacy: null
-        }
-        this.handleSettingsClick = this.handleSettingsClick.bind(this);
     }
+    this.handleSettingsClick = this.handleSettingsClick.bind(this);
+    }
+///////////////////////////////////////////////////
+   async componentDidMount() {
+        let userId = localStorage.getItem('uID');
+        let settings = await this.props.fetchSettings(userId);
+        if (settings) {
+        this.setState({
+            sms: this.props.settings[0].sms,
+            privacy: this.props.settings[0].privacy
+        })
+    }
+    }
+
+    ///////////////////////////////////////////////////////////
+
     //to render these individaully will need to set reducer state for each one ie pastservicesShowingDAsh ==true then set to false on others
     //when calling the other to render so conditionals show only one at a time
     handlePastServicesClick = () => {
@@ -58,7 +72,7 @@ class FeedSub extends React.Component {
         this.props.fetchBookings(userId);
     }
 
-   async handleSettingsClick () {
+    async handleSettingsClick () {
         this.setState({
             fetchedUserInfo: false,
             fetchedBookings: false,
@@ -95,16 +109,7 @@ class FeedSub extends React.Component {
         })
     }
 
-    // handleSmsClick = () => {
-    //     const userId = localStorage.getItem('uID');
-    //     let body = {}
-    //     if (this.props.settings.sms === false) {
-    //         body.sms = true
-    //     } else if (this.props.settings.sms === true) {
-    //         body.sms = false
-    //     }
-    //     this.props.putSms(userId, body);
-    // }
+  
 
     handleSettingsToggleClick = (e) => {
         const userId = localStorage.getItem('uID');
@@ -118,7 +123,13 @@ class FeedSub extends React.Component {
         this.props.putSettings(userId, body);
         this.props.fetchSettings(userId);
     }
-
+/////////////////////////////////////////
+    handleToggleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+////////////////////////////////////////////
 
 
 
@@ -177,14 +188,14 @@ class FeedSub extends React.Component {
                             <div className="checkWrappper">
                                 <p className="settingsP">Privacy:</p><p className="settingsdes">click to block geolocation services when not in the service time window</p> 
                                 <label className="switch">
-                                    <input type="checkbox" checked={this.props.settings[0].privacy} name='privacy' onClick={this.handleSettingsToggleClick}/>
+                                    <input type="checkbox" checked={this.state.privacy} name='privacy' value={this.state.privacy} onClick={this.handleSettingsToggleClick} onChange={this.handleToggleChange}/>
                                     <span className="slider round"></span>
                                 </label>
                             </div>
                             <div className="checkWrappper">
                                 <p className="settingsP">SMS:</p><p className="settingsdes">click to block SMS notifications</p>
                                 <label className="switch">
-                                    <input type="checkbox" checked={this.props.settings[0].sms} name="sms" onClick={this.handleSettingsToggleClick}/>
+                                    <input type="checkbox" checked={this.state.sms} name="sms" value={this.state.sms} onClick={this.handleSettingsToggleClick} onChange={this.handleToggleChange}/>
                                     <span className="slider round"></span>
                                 </label>
                             </div>
