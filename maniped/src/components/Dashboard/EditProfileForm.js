e8a20b0d92c409a41a1a854e9f7afd0e6ef398cb
Ali Rajaii <rajaii_a@yahoo.com>
2020-08-12 17:25:09 -0400

    EditProfileForm.js bare bones added and pseudo on feedsub

---
+++ maniped/src/components/Dashboard/EditProfileForm.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { connect } from 'react-redux';
+
+function EditProfileForm (props) {
+
+}
+
+const mapStateToProps = state => {
+    return {
+        
+    }
+}
+
+export default connect(mapStateToProps, {editProfile})(EditProfileForm);
