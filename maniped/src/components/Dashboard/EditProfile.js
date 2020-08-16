ffe4f2b8902587df96bc9f77fd5a367cead156e1
Ali Rajaii <rajaii_a@yahoo.com>
2020-08-16 14:56:06 -0400

    debugged validation for edit profile in settings

--- maniped/src/components/Dashboard/EditProfile.js
+++ maniped/src/components/Dashboard/EditProfile.js
@@ -27,15 +27,15 @@ class EditProfile extends React.Component {
     return (
     <div className='editProfileWrapper'>
         <p onClick={this.openForm} name='editingUsername' value={this.state.editingUsername} className='editProfile'>Username: {this.props.username}</p>
-        {this.state.editingUsername && <EditProfileForm thing='username' name='username'/>}
+        {this.state.editingUsername && <EditProfileForm closeEdit={this.props.closeEdit} thing='username' name='username'/>}
         <p onClick={this.openForm} name='editingEmail' value={this.state.editingEmail} className='editProfile'>Email: {this.props.email}</p>
-        {this.state.editingEmail && <EditProfileForm thing='email' name='email'/>}
+        {this.state.editingEmail && <EditProfileForm closeEdit={this.props.closeEdit} thing='email' name='email'/>}
         <p onClick={this.openForm} name='editingPhoneNumber' value={this.state.editingPhoneNumber} className='editProfile'>Phone number: {this.props.phone_number}</p>
-        {this.state.editingPhoneNumber && <EditProfileForm thing='phone number' name='phone_number'/>}
+        {this.state.editingPhoneNumber && <EditProfileForm  closeEdit={this.props.closeEdit}thing='phone number' name='phone_number'/>}
         <p onClick={this.openForm} name='editingPrimaryAddress' value={this.state.editingPrimaryAddress} className='editProfile'>Primary address: {this.props.address}</p>
-        {this.state.editingPrimaryAddress && <EditProfileForm thing='primary address' name='address'/>}
+        {this.state.editingPrimaryAddress && <EditProfileForm closeEdit={this.props.closeEdit} thing='primary address' name='address'/>}
         <p onClick={this.openForm} name='editingZip' value={this.state.editingZip} className='editProfile'>Primary zipcode: {this.props.zipcode}</p>
-        {this.state.editingZip && <EditProfileForm thing='zipcode' name='zipcode'/>}
+        {this.state.editingZip && <EditProfileForm closeEdit={this.props.closeEdit} thing='zipcode' name='zipcode'/>}
     </div>
     )
     }
