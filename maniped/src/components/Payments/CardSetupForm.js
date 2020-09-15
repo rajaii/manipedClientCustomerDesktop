6241d9b927179580b8beff5176b7e4d020c02a5c
Ali Rajaii <rajaii_a@yahoo.com>
2020-09-15 16:25:35 -0400

    flow to add card info added, next work on functionality

---
+++ maniped/src/components/Payments/CardSetupForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js';
+
+import CardSection from './CardSection';
+
+export default function CardSetupForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const result = await stripe.confirmCardSetup('{{CLIENT_SECRET}}', {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Jenny Rosen',
+        },
+      }
+    });
+
+    if (result.error) {
+      // Display result.error.message in your UI.
+    } else {
+      // The setup has succeeded. Display a success message and send
+      // result.setupIntent.payment_method to your server to save the
+      // card to a Customer
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardSection />
+      <button disabled={!stripe}>Save Card</button>
+    </form>
+  );
+}
--- maniped/src/index.js
+++ maniped/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
@@ -11,14 +15,17 @@ import './index.css';
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const stripePromise = loadStripe("pk_test_51HPaoQFSPGPUh8ixB4uWwv7sokfnQTRZ2bXeJhrtre0msDJOo1YpOYgtnT0U5D6q2SipDKOuVrP1PINLIBuYXKJj00pdiCiZpS");
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>, document.getElementById('root')
+  <Elements stripe={stripePromise}>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </Elements>, document.getElementById('root')
 );
 
 
