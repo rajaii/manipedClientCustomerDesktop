d46eaf146eaf08aae31acc63c23de6908b972ff4
Ali Rajaii <rajaii_a@yahoo.com>
2020-09-16 17:49:09 -0400

    stripe add card working, now need to work on saving customer card object in db

--- maniped/src/components/Payments/CardSetupForm.js
+++ maniped/src/components/Payments/CardSetupForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js';
 
 import CardSection from './CardSection';
@@ -17,23 +18,38 @@ export default function CardSetupForm() {
       // Make sure to disable form submission until Stripe.js has loaded.
       return;
     }
-
-    const result = await stripe.confirmCardSetup('{{CLIENT_SECRET}}', {
-      payment_method: {
-        card: elements.getElement(CardElement),
-        billing_details: {
-          name: 'Jenny Rosen',
-        },
+    const userId = localStorage.getItem('uID');
+    console.log(userId)
+   axios.get(`http://localhost:4000/card-wallet`)
+    .then( async res => {
+      console.log(res)
+      let secret = res.data.client_secret;
+      const result = await stripe.confirmCardSetup(secret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: {
+              //get name in V
+            name: 'Ali',
+          },
+        }
+      });
+  
+      if (result.error) {
+        console.log('here:', result.error)
+      } else {
+        // The setup has succeeded. Display a success message and send
+        // result.setupIntent.payment_method to your server to save the
+        // card to a Customer
+        console.log('success');
       }
-    });
+    })
+    .catch(err => {
+      console.log(err)
+    })
 
-    if (result.error) {
-      // Display result.error.message in your UI.
-    } else {
-      // The setup has succeeded. Display a success message and send
-      // result.setupIntent.payment_method to your server to save the
-      // card to a Customer
-    }
+   
+
+    
   };
 
   return (
