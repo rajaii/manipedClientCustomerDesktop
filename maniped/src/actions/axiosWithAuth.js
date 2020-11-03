e100356840999450611f9af70844fe631bfa4482
Ali Rajaii <rajaii_a@yahoo.com>
2020-11-02 19:03:34 -0500

    chrome sending malformed tokens.  set xcustomheaders in the logic to get around this.  will test further to see if is just my chrome or what

--- maniped/src/actions/axiosWithAuth.js
+++ maniped/src/actions/axiosWithAuth.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export default function() {
     const token = localStorage.getItem('token');
-
+    console.log(token)
     return axios.create({
         headers : {
             'Content-Type': 'application/json',
-            'Authorization': `${token}`,
+            'xcustomheaders': `${token}`,
         }
     })
 }
